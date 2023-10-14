f4d6dd1b0c4c266cde455a981c9d4644068508d3
Shrimpy48 <45856863+Shrimpy48@users.noreply.github.com>
2023-10-14 15:11:32 +0100

    Move tree-sitter parser to separate repo

--- tree-sitter-lambda/queries/locals.scm
+++
@@ -1,7 +0,0 @@
-(abstraction) @local.scope
-(product) @local.scope
-
-(abstraction bound:(variable) @local.definition)
-(product input_name:(variable) @local.definition)
-
-(variable) @local.reference
